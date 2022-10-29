778a31c3d8462e0591c54bf7cf91ac018e55c8bc
Marcelo Lima <m-lima@users.noreply.github.com>
2022-10-29 18:39:23 +0200

    Print span on entry

--- src/log/layer.rs
+++ src/log/layer.rs
@@ -57,6 +57,21 @@ impl tracing_subscriber::Layer<tracing_subscriber::Registry> for Layer {
             if span.extensions().get::<SpanInfo>().is_none() {
                 span.extensions_mut().insert(SpanInfo::new(attrs));
             }
+
+            let mut stdout = std::io::stdout().lock();
+
+            let depth = ctx.span_scope(id).map_or(0, std::iter::Iterator::count);
+            let last_span = self.last_span.load(std::sync::atomic::Ordering::Relaxed);
+
+            print_span(
+                &mut stdout,
+                last_span,
+                depth.max(1) - 1,
+                Some(span).as_ref(),
+            );
+
+            self.last_span
+                .store(id.into_u64(), std::sync::atomic::Ordering::Relaxed);
         }
     }
 
